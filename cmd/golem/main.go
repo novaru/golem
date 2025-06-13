791178b156bb1af786f9952cc9f4036f1b297878
Febriyan Ahza <69071485+novaru@users.noreply.github.com>
2025-06-14 04:49:26 +0700

    Initial commit

---
+++ cmd/golem/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Golem is a load balancer for HTTP services.")
+	fmt.Println("It is designed to be simple, fast, and easy to use.")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/novaru/golem
+
+go 1.24.4
